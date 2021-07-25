ebcfb8777da5a639f5ac6ec80eab758b78a7c401
Cliff Wulfman <cwulfman@princeton.edu>
2021-07-25 19:03:27 -0400

    code implementation

---
+++ src/statuscodes.js
@@ -0,0 +1,11 @@
+/** statusCodes is an object representing the various status codes
+ * returned by the av client
+ */
+const statusCodes = {
+    success: 'success',
+    invalid: 'invalid',
+    networkError: 'network error',
+    expired: 'expired'
+}
+
+exports.statusCodes = statusCodes;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-const avclient = require('../avclient');
+const avclient = require('../src/avclient');
+const statusCodes = require('../src/statusCodes').statusCodes;
 const expect = require('chai').expect;
 
 
@@ -6,25 +7,25 @@ const expect = require('chai').expect;
 describe('avclient', function () {
     describe('AccessCodeRequest', function () {
 	it('returns `network error` if ID# is all zeros', function() {
-	    expect(avclient.AccessCodeRequest('Jane', 'Doe', '01-01-2001','0')).to.equal('network error');
-	    expect(avclient.AccessCodeRequest('John', 'Doe', '01-01-2001','000')).to.equal('network error');
+	    expect(avclient.AccessCodeRequest('Jane', 'Doe', '01-01-2001','0')).to.equal(statusCodes.networkError);
+	    expect(avclient.AccessCodeRequest('John', 'Doe', '01-01-2001','000')).to.equal(statusCodes.networkError);
 	});
 
 	it('returns `success` otherwise', function () {
-	    expect(avclient.AccessCodeRequest('Jane', 'Doe', '01-01-2001','12345')).to.equal('success');
+	    expect(avclient.AccessCodeRequest('Jane', 'Doe', '01-01-2001','12345')).to.equal(statusCodes.success);
 	});
     });
 
     describe('AccessCodeConstruct status', function () {
 	let CVR = '_';
 	it('returns `invalid` if accessCode is `00000`', function () {
-	    expect(avclient.AccessCodeConstruct(CVR, '00000').Status).to.equal('invalid')
+	    expect(avclient.AccessCodeConstruct(CVR, '00000').Status).to.equal(statusCodes.invalid)
 	});
 	it('returns `expired` if accessCode is `00001`', function () {
-	    expect(avclient.AccessCodeConstruct(CVR, '00001').Status).to.equal('expired')
+	    expect(avclient.AccessCodeConstruct(CVR, '00001').Status).to.equal(statusCodes.expired)
 	});
 	it('returns `network error` if accessCode is `00002`', function () {
-	    expect(avclient.AccessCodeConstruct(CVR, '00002').Status).to.equal('network error')
+	    expect(avclient.AccessCodeConstruct(CVR, '00002').Status).to.equal(statusCodes.networkError)
 	});
 	it('returns `success` if accessCode is anything else', function () {
 	    var fprint = avclient.AccessCodeConstruct(CVR, '12345');
@@ -45,17 +46,17 @@ describe('avclient', function () {
 	it('returns `network error` if the current access code is `00003`',
 	   function() {
 	       avclient.currentCode('00003');
-	       expect(avclient.Spoil(fingerprint)).to.equal('network error');
+	       expect(avclient.Spoil(fingerprint)).to.equal(statusCodes.networkError);
 	   });
 	if('returns `invalid` if the current access code is `00004`',
 	   function() {
 	       avclient.currentCode('00004');
-	       expect(avclient.Spoil(fingerprint)).to.equal('invalid');
+	       expect(avclient.Spoil(fingerprint)).to.equal(statusCodes.invalid);
 	   });
 	if('returns `success` for everything else',
 	   function() {
 	       avclient.currentCode('999999');
-	       expect(avclient.Spoil(fingerprint)).to.equal('success');
+	       expect(avclient.Spoil(fingerprint)).to.equal(statusCodes.success);
 	   });
     });
 
@@ -64,31 +65,20 @@ describe('avclient', function () {
 	it('returns `network error` if the current access code is `00005`',
 	   function() {
 	       avclient.currentCode('00005');
-	       expect(avclient.Send(_,_,_)).to.equal('network error');
+	       expect(avclient.Send(_,_,_)).to.equal(statusCodes.networkError);
 	   });
 	if('returns `invalid` if the current access code is `00006`',
 	   function() {
 	       avclient.currentCode('00004');
-	       expect(avclient.Send(_,_,_)).to.equal('invalid');
+	       expect(avclient.Send(_,_,_)).to.equal(statusCodes.invalid);
 	   });
 	if('returns `success` for everything else',
 	   function() {
 	       avclient.currentCode('999999');
-	       expect(avclient.Send(_,_,_)).to.equal('success');
+	       expect(avclient.Send(_,_,_)).to.equal(statusCodes.success);
 	   });
     });
     
-    describe('toggle', function() {
-	it('toggles', function() {
-	    let a = avclient.Toggle()
-	    let b = avclient.Toggle()
-	    let c = avclient.Toggle()
-	    expect(a).to.equal(0)
-	    expect(b).to.equal(1)
-	    expect(c).to.equal(0)
-	});
-    });
-
     describe('currentCode', function() {
 	it('returns stashed value', function() {
 	    avclient.currentCode('123');	    
